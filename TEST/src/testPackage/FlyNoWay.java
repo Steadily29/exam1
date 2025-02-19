c0bf3735976ea9961ef0855f49676228cad84a69
hong <hong@10.101.101.150>
2025-02-19 17:26:23 +0900

    수정작업

--- TEST/src/testPackage/FlyNoWay.java
+++ TEST/src/testPackage/FlyNoWay.java
@@ -5,7 +5,8 @@ public class FlyNoWay implements FlyBehavior {
 	@Override
 	public void fly() {
 		System.out.println("날지 못해요.");
-		System.out.println("수정했다운");
+		System.out.println("노내가수정2했다우");
+		System.out.println("수정했다운123");
 	}
 
 }
