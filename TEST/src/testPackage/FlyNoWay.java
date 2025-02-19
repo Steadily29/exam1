7ff25255ebfadbc0ccd2aba7b1e31cde7eff6b4d
Steadily29 <104519861+Steadily29@users.noreply.github.com>
2025-02-19 17:19:17 +0900

    다시 지웠다

--- TEST/src/testPackage/FlyNoWay.java
+++ TEST/src/testPackage/FlyNoWay.java
@@ -5,7 +5,6 @@ public class FlyNoWay implements FlyBehavior {
 	@Override
 	public void fly() {
 		System.out.println("날지 못해요.");
-		System.out.println("노내가수정했다우");
 		System.out.println("수정했다운");
 	}
 
