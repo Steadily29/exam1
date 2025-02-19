869e9229e1528a8a69fe219d8ffe6e599b040747
Steadily29 <104519861+Steadily29@users.noreply.github.com>
2025-02-19 16:30:32 +0900

    수정했다운

--- TEST/src/testPackage/FlyNoWay.java
+++ TEST/src/testPackage/FlyNoWay.java
@@ -5,7 +5,7 @@ public class FlyNoWay implements FlyBehavior {
 	@Override
 	public void fly() {
 		System.out.println("날지 못해요.");
-		System.out.printLn("노운");
+		System.out.println("수정했다운");
 	}
 
 }
